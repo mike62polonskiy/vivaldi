141e74cc3679eff74f704524f2a29b96d5c87d99
Mike Polonskiy <kenny62rzn17@gmail.com>
2020-01-10 23:58:52 +0300

    добавил модели в админку

--- src/project/settings.py
+++ src/project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vk_data_grub'
+    'vk_data_grub',
 ]
 
 MIDDLEWARE = [
--- src/vk_data_grub/admin.py
+++ src/vk_data_grub/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.VkGroups)
+class VkGroups(models.VkGroups):
+    show_full_result_count = False
+
+@admin.register(models.Events)
+class Events(models.Events):
+    show_full_result_count = False
